e697edbe14366d24eed3f1fb04faf90806ec7d7b
hailey-tam-ics4u1-03-25 <hailey.tam@student.tdsb.on.ca>
2025-02-24 23:03:34 +0000

    Completed Code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,40 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+	
 	}
 
+	public static int[] createIntArray(){
+		int arr[] = new int[5];
+			for (int i = 0; i < arr.length; i++){
+				arr[i] = i + 1;
+		}
+		return arr;
+	}
+	public static String[] createArray(String a, String b, String c, String d) {
+		String stringArr[] = {a, b, c, d};
+		return stringArr;
+	}
+	
+	public static int findValue(int num1, int[] arr2){
+		for (int i = 0; i < arr2.length; i++) {
+			if (arr2[i] == num1){
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	public static int findThirdValue(String word, String[] wordArr){
+		int occurance = 0;
+		for (int i = 0; i < wordArr.length; i++){
+			if (wordArr[i] == word){
+				occurance++;
+				if (occurance == 3){
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 }
